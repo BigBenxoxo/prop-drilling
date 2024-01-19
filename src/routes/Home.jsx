191e150081754f532fb4b7b2835db8788da44f80
BenPet <bendzuke@gmail.com>
2024-01-19 14:11:21 +0200

    Updating working

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,20 @@
 import { Typography } from "@mui/material";
 import { url } from "../api/api";
 import ComponentA from "../components/ComponentA";
-import { getData } from "../services/apiRequests";
-
-const data = await getData(url);
-
-console.log("Data: ", data);
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getDataRedux } from "../store/dataSlice";
+// const data = await getData(url);
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => state.data);
+  console.log("Data: ", data);
+
+  useEffect(() => {
+    dispatch(getDataRedux(url));
+  }, [dispatch]);
+
   return (
     <div>
       <Typography variant="h6" component="h2">
